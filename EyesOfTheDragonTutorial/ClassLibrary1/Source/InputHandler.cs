583051a8fb7816976fb99cbd827e424be1294035
Samuel Hindmarsh <Samuel.Hindmarsh@gmail.com>
2013-03-03 22:51:11 +1300

    Working through tutorial #1 (up to page 7, the GameStateManager class)

---
+++ EyesOfTheDragonTutorial/ClassLibrary1/Source/InputHandler.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XRpgLibrary.Source
+{
+    public class InputHandler : GameComponent
+    {
+        #region Fields
+
+        static KeyboardState keyboardState;
+        static KeyboardState lastKeyboardState;
+
+        #endregion
+
+        #region Properties
+
+        public static KeyboardState KeyboardState
+        {
+            get { return keyboardState; }
+        }
+
+        public static KeyboardState LastKeyboardState
+        {
+            get { return lastKeyboardState; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public InputHandler(Game game) : base(game)
+        {
+            keyboardState = Keyboard.GetState();
+        }
+
+        #endregion
+
+        #region XNA methods
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            lastKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+            
+            base.Update(gameTime);
+        }
+
+        #endregion
+
+        #region Other methods
+
+        public static void Flush(){
+            lastKeyboardState = keyboardState;
+        }
+
+        #endregion
+
+        #region Keyboard methods
+
+        public static bool KeyReleased(Keys key)
+        {
+            return keyboardState.IsKeyUp(key) && lastKeyboardState.IsKeyDown(key);
+        }
+
+        public static bool KeyPressed(Keys key)
+        {
+            return keyboardState.IsKeyDown(key) && lastKeyboardState.IsKeyUp(key);
+        }
+
+        public static bool KeyDown(Keys key)
+        {
+            return keyboardState.IsKeyDown(key);
+        }
+
+        #endregion
+
+    }
+}
---
+++ EyesOfTheDragonTutorial/ClassLibrary1/Source/States/GameState.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XRpgLibrary.Source.States
+{
+    public abstract partial class GameState : DrawableGameComponent
+    {
+        #region Fields
+
+        List<GameComponent> childComponents;
+        GameState tag;
+        protected GameStateManager StateManager;
+
+        #endregion
+
+        #region Properties
+
+        public List<GameComponent> Components
+        {
+            get { return childComponents; }
+        }
+
+        public GameState Tag
+        {
+            get { return tag; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public GameState(Game game, GameStateManager manager)
+            : base(game)
+        {
+            StateManager = manager;
+            childComponents = new List<GameComponent>();
+            tag = this;
+        }
+
+        #endregion
+
+        #region XNA methods
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (GameComponent component in childComponents)
+            {
+                if (component.Enabled)
+                {
+                    component.Update(gameTime);
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            DrawableGameComponent drawComponent;
+
+            foreach (GameComponent component in childComponents)
+            {
+                if (component is DrawableGameComponent)
+                {
+                    drawComponent = component as DrawableGameComponent;
+                    if (drawComponent.Visible)
+                    {
+                        drawComponent.Draw(gameTime);
+                    }
+                }
+            }
+            
+            base.Draw(gameTime);
+        }
+
+        #endregion
+
+        #region GameState methods
+
+        internal protected virtual void StateChange(object sender, EventArgs e)
+        {
+            if (StateManager.CurrentState == Tag)
+            {
+                Show();
+            }
+            else
+            {
+                Hide();
+            }
+        }
+
+        protected virtual void Show()
+        {
+            Visible = true;
+            Enabled = true;
+
+            foreach (GameComponent component in childComponents)
+            {
+                component.Enabled = true;
+                if (component is DrawableGameComponent)
+                {
+                    ((DrawableGameComponent)component).Visible = true;
+                }
+            }
+        }
+
+        protected virtual void Hide()
+        {
+            Visible = false;
+            Enabled = false;
+
+            foreach (GameComponent component in childComponents)
+            {
+                component.Enabled = false;
+                if (component is DrawableGameComponent)
+                {
+                    ((DrawableGameComponent)component).Visible = false;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
